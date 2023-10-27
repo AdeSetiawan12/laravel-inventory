2ef471b927227e9b888d90068b4c79cf8622bf6a
AdeSetiawan12 <setiawan.ade73@gmail.com>
2023-10-27 21:15:48 +0700

    master barang: prosesm simpan ke db

--- resources/views/master/barang/form-tambah.blade.php
+++ resources/views/master/barang/form-tambah.blade.php
@@ -2,17 +2,18 @@
 @section('master-konten')
 
 <form action="{{ route('master-barang-simpan')}}" method="post">
+    @csrf 
     <div class="mb-3">
         <label for="html_kode" class="form-label">Kode</label>
-        <input type="text" class="form-control w-50" id="html_kode" placeholder="Kode Barang">
+        <input type="text" class="form-control w-50" id="html_kode" name="html_kode" placeholder="Kode Barang">
     </div>
     <div class="mb-3">
         <label for="html_nama" class="form-label">Nama</label>
-        <input type="text" class="form-control w-50" id="html_nama" placeholder="Nama Barang">
+        <input type="text" class="form-control w-50" id="html_nama" name="html_nama" placeholder="Nama Barang">
     </div>
     <div class="mb-3">
         <label for="html_deskripsi" class="form-label">Deskripsi</label>
-        <textarea class="form-control w-50" id="html_deskripsi" rows="4"></textarea>
+        <textarea class="form-control w-50" id="html_deskripsi" name="html_deskripsi" rows="4"></textarea>
     </div>
 
     <button type="submit" class="btn btn-primary">
